a1234ee7a8a0718c187442695490bec5aa97027c
Goose <akoverofeev@gmail.com>
2026-02-21 21:35:33 +0300

    Update ShipyardSystem.Console.cs

--- Content.Server/_Eclipse/Shuttles/Systems/ShipyardSystem.Console.cs
+++ Content.Server/_Eclipse/Shuttles/Systems/ShipyardSystem.Console.cs
@@ -149,7 +149,5 @@ public sealed partial class ShipyardSystem
                 });
             }
         }
-
-        _userInterface.SetUiState(ent.Owner, ShipyardConsoleUiKey.Key, state);
     }
 }
